f0e4317b32bd775a855346d3833a1e27de651d71
NickHemerycke <safrynick@gmail.com>
2025-05-22 12:22:57 +0200

    setting up basic models

--- webProject/BingeBox/models.py
+++ webProject/BingeBox/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.username
+    
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.title
